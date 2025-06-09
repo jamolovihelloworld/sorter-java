5546fc6577ca6d29dd69e59b08d1af0b365321ae
JAMOLOVI <jamolbekjamolov50@gmail.com>
2025-06-09 10:04:23 +0500

    Загрузка проекта на Java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 8, 1};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
